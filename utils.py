8c26f6552dec5d276779046346266f2da81fedd2
Octopustank <2745865945@qq.com>
2024-10-08 14:16:32 +0800

    auto key file generate

--- utils.py
+++ utils.py
@@ -54,7 +54,6 @@ def check_workdir() -> None:
         condition_assert(f"Data path({DATA_PATH}) is not exist.\nIt has created automatically, but missing data files.")
     condition_assert(f"Home path({HOME_ROOT}) is not exist", os.path.isdir(HOME_ROOT))
     condition_assert(f"Public path({PUBLIC_PATH}) is not exist", os.path.isdir(PUBLIC_PATH))
-    condition_assert(f"Key path({KEY_PATH}) is not exist", os.path.isfile(KEY_PATH))
     if not os.path.isfile(KEY_PATH):
         write_file(KEY_PATH, {})
     try:
